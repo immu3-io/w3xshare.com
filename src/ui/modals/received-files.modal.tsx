399f2bf958800f63d9a91d3c0f2d631f27f234a4
David Tacer <david.tacer@data-vallis.com>
2024-04-18 10:02:39 +0200

    fetch method key bug fix

--- src/ui/modals/received-files.modal.tsx
+++ src/ui/modals/received-files.modal.tsx
@@ -27,25 +27,13 @@ const ReceivedFilesModal: FC<IReceivedFilesModalProps> = ({ show, onClose, txHas
 
   const handleFetchFilesOnClick = async (event): Promise<void> => {
     event.preventDefault()
-    console.log('tu')
-    if (!formRef.current.checkValidity()) {
-      formRef.current.reportValidity()
-      return
-    }
 
     setShowSecretKeyInput(false)
 
     try {
-      await initMail(
-        formRef.current.secretKey.value.trim(),
-        account.nfts[account.defaultNftIndex].endpoint,
-        account.nfts[account.defaultNftIndex].jwt,
-        chain.id
-      )
-      console.log(formRef.current.secretKey.value.trim(), account.nfts[account.defaultNftIndex].endpoint, account.nfts[account.defaultNftIndex].jwt, chain.id)
+      const sKey = formRef.current.secretKey.value.trim() || secretKey.trim()
+      await initMail(sKey, account.nfts[account.defaultNftIndex].endpoint, account.nfts[account.defaultNftIndex].jwt, chain.id)
       const receivedEnvelope = await mail.fetchByTransactionHash(txHash)
-      console.log(receivedEnvelope)
-      console.log(receivedEnvelope)
 
       setFetching(false)
       setFetchingText('noFiles')
