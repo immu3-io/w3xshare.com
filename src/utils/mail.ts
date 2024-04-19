480e6dc4760415b7ad43384c2ea2f2be73f2f309
David Tacer <david.tacer@data-vallis.com>
2024-04-19 10:04:33 +0200

    new SC address mail

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -39,13 +39,13 @@ const immu3Testnet: Chain = {
   networkEndpoint: `https://fraa-flashbox-2800-rpc.a.stagenet.tanssi.network/`,
   contracts: {
     appFeeManager: {
-      address: '0xE459E555f0CCd996F03A601158eEAe6deC4633bC'
+      address: '0x1130412f89E2C5fc4B76dadDa9c555D7ccac2AB7'
     },
     mail: {
-      address: '0xa6199D54df4c904976DC1741eE75A9570c7A3308'
+      address: '0x2B568A7D93DdDe2cbEE293013e80617ab60Efd80'
     },
     user: {
-      address: '0x5aCeed5372E91C1EB2C09E0F5C46B29A282b2C2D'
+      address: '0x6625570b2d3B8C5d0Fb9b5Ac812462Cc18365914'
     }
   }
 }
